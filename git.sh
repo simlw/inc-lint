f231d9891cabe425b2a774d18368ea7ec982bee9
刘伟 <hzliuwei@corp.netease.com>
2018-04-22 14:41:16 +0800

    Added: color result

--- git.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/bash
-HEAD=`git rev-parse HEAD`
-HEAD1=`git rev-parse HEAD~1`
-git diff $HEAD $HEAD1 --name-status
--- index.js
+++ index.js
@@ -1,35 +1,24 @@
 "use strict";
 
-var exec = require('child_process').exec;
 var commander = require('commander');
 var path = require('path');
-var gitPath = path.resolve(__dirname, 'git.sh');
+var shell = require('shelljs');
+var color = require('./lib/colorResult');
+var getChangedFiles = require('./lib/filesFinder');
 
-function execCommand(command, callback) {
-  var childProcess = exec(command, function(err, stdout, stderr) {
-    if (err) return callback(new Error(err), null);
-    if (typeof stderr !== 'string') return callback(new Error(stderr), null);
-    return callback(null, stdout);
-  });  
+function list(value) {
+  if (!value) return [];
+  return value.split(',');
 }
 
 commander.version('1.0.0')
   .option('-c --config <path>', 'config path. defaults to ./')
+  .option('--targetDir <items>', 'target dir to run eslint', list)
   .parse(process.argv);
 
-execCommand(`bash ${gitPath}`, function(err, result) {
-  if (err) return console.error(err);
-  const srcFiles = result.split('\n');
-  const changedFiles = [];
-  for (let i = 0, max = srcFiles.length; i < max; i +=1) {
-    if (srcFiles[i]) {
-      changedFiles.push(srcFiles[i].replace(/(^[D|M|A])|\t|\r|\n/g, ''));
-    }
-  }
+var changeFiles = getChangedFiles(commander.targetDir);
+console.log('====>> change files: ', changeFiles);
+var command = `eslint -c ${commander.config} ${changeFiles.join(' ')}`; 
+var result = shell.exec(command, { silent: true }).stdout;
 
-  const command = `eslint -c ${commander.config} ${changedFiles.join(' ')}`; 
-  exec(command, function(err, stdout, stderr) {
-    if (err) console.error(err);
-    console.log(stdout);
-  });
-});
+console.log(color(result));

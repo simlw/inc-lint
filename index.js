694755c9af3c12f0cacd9f12fa1cb332be8f042c
刘伟 <hzliuwei@corp.netease.com>
2018-05-11 19:36:22 +0800

    Fixed: fixed can be submitted with errors

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 "use strict";
 
 var commander = require('commander');
-var path = require('path');
-var shell = require('shelljs');
-var color = require('./lib/colorResult');
-var getChangedFiles = require('./lib/filesFinder');
+const cli = require('./lib/cli');
 
 function list(value) {
   if (!value) return [];
@@ -16,9 +13,4 @@ commander.version('1.0.0')
   .option('--targetDir <items>', 'target dir to run eslint', list)
   .parse(process.argv);
 
-var changeFiles = getChangedFiles(commander.targetDir);
-if (!changeFiles || changeFiles.length === 0) return;
-var command = `eslint -c ${commander.config} ${changeFiles.join(' ')}`; 
-var result = shell.exec(command, { silent: true }).stdout;
-
-console.log(color(result));
+process.exitCode = cli.execute(commander);
